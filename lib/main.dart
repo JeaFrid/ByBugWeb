87203f835f07af0782731d36b51b253377acdb80
JeaFriday <jeafriday@gmail.com>
2023-02-23 21:03:00 +0300

    23-02-2023 21:02

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: LayoutBuilder(
           builder: (p0, p1) {
-            return RegisterPage();
+            return const HomePage();
           },
         ),
       ),
--- lib/page/auth/register.dart
+++ lib/page/auth/register.dart
@@ -227,168 +227,173 @@ class RegisterPage extends StatelessWidget {
 
   Scaffold mobile() {
     return Scaffold(
-        backgroundColor: ThemeColors().backgroundColor,
-        body: LayoutBuilder(
-          builder: (context, constraints) {
-            return Center(
-              child: SingleChildScrollView(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Image.asset("assets/images/logo-space.png",
-                            width: 100),
-                      ),
-                      Column(
+      backgroundColor: ThemeColors().backgroundColor,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Center(
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Image.asset("assets/images/logo-space.png",
+                          width: 100),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "ByBugWeb'e Kayıt Ol!",
+                          style: GoogleFonts.poppins(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Software&Development",
+                          style: GoogleFonts.poppins(
+                            fontSize: 12,
+                            color: Colors.white70,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: constraints.maxWidth - 100,
+                      child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Text(
-                            "ByBugWeb'e Kayıt Ol!",
-                            style: GoogleFonts.poppins(
-                              fontSize: 25,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
+                          CustomTextField(
+                              type: 4,
+                              textfieldController: nameController,
+                              labelText: "Kullanıcı Adı"),
+                          CustomTextField(
+                              type: 3,
+                              textfieldController: emailController,
+                              labelText: "E-posta"),
+                          CustomTextField(
+                            type: 2,
+                            textfieldController: passwordController,
+                            labelText: "Parola",
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "Kayıt olarak ByBug'ın Gizlilik Sözleşmesini kabul etmiş olursunuz.",
+                              style: GoogleFonts.openSans(
+                                fontSize: 12,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
-                          Text(
-                            "Software&Development",
-                            style: GoogleFonts.poppins(
-                              fontSize: 12,
-                              color: Colors.white70,
-                              fontWeight: FontWeight.bold,
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            width: constraints.maxWidth / 2,
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            color: const Color.fromARGB(10, 255, 255, 255),
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(5),
+                              onTap: () async {
+                                List messageList =
+                                    await FirebaseEditor.register(
+                                  emailController.text,
+                                  passwordController.text,
+                                  [
+                                    "https://raw.githubusercontent.com/JeaFrid/ByBugWeb/master/assets/images/logo-classic.png",
+                                    nameController.text,
+                                    emailController.text,
+                                    passwordController.text,
+                                    nowTime,
+                                    "Türkiye"
+                                  ],
+                                );
+                                messageList[0] == "1"
+                                    ? Get.offAll(const ProfilePage())
+                                    // ignore: use_build_context_synchronously
+                                    : ByBugDialg.error(
+                                        context,
+                                        constraints,
+                                        "Kayıt Başarısız!",
+                                        messageList[1].toString());
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 16),
+                                child: Text(
+                                  "Kayıt Ol",
+                                  style: GoogleFonts.openSans(
+                                    fontSize: 14,
+                                    color: Colors.white70,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: constraints.maxWidth / 2,
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 5),
+                            color: const Color.fromARGB(10, 255, 255, 255),
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(5),
+                              onTap: () {
+                                Get.to(
+                                  LoginPage(),
+                                );
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 16),
+                                child: Text(
+                                  "Hesabınız var mı?",
+                                  style: GoogleFonts.openSans(
+                                    fontSize: 14,
+                                    color: Colors.white70,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(12),
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(5),
+                              onTap: () {
+                                Get.back();
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.all(16),
+                                child: Text(
+                                  "Eve Dön",
+                                  style: GoogleFonts.openSans(
+                                    fontSize: 14,
+                                    color: Colors.white70,
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
                         ],
                       ),
-                      SizedBox(
-                        width: constraints.maxWidth - 100,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            CustomTextField(
-                                type: 4,
-                                textfieldController: nameController,
-                                labelText: "Kullanıcı Adı"),
-                            CustomTextField(
-                                type: 3,
-                                textfieldController: emailController,
-                                labelText: "E-posta"),
-                            CustomTextField(
-                              type: 2,
-                              textfieldController: passwordController,
-                              labelText: "Parola",
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                "Kayıt olarak ByBug'ın Gizlilik Sözleşmesini kabul etmiş olursunuz.",
-                                style: GoogleFonts.openSans(
-                                  fontSize: 12,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            Container(
-                              width: constraints.maxWidth / 2,
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 5),
-                              color: const Color.fromARGB(10, 255, 255, 255),
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(5),
-                                onTap: () async {
-                                  List messageList =
-                                      await FirebaseEditor.register(
-                                    emailController.text,
-                                    passwordController.text,
-                                    [
-                                      "https://raw.githubusercontent.com/JeaFrid/ByBugWeb/master/assets/images/logo-classic.png",
-                                      nameController.text,
-                                      emailController.text,
-                                      passwordController.text,
-                                      nowTime,
-                                      "Türkiye"
-                                    ],
-                                  );
-                                  messageList[0] == "1"
-                                      ? Get.offAll(const ProfilePage())
-                                      // ignore: use_build_context_synchronously
-                                      : ByBugDialg.error(
-                                          context,
-                                          constraints,
-                                          "Kayıt Başarısız!",
-                                          messageList[1].toString());
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 16, vertical: 16),
-                                  child: Text(
-                                    "Kayıt Ol",
-                                    style: GoogleFonts.openSans(
-                                      fontSize: 14,
-                                      color: Colors.white70,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: constraints.maxWidth / 2,
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 5),
-                              color: const Color.fromARGB(10, 255, 255, 255),
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(5),
-                                onTap: () {
-                                  Get.to(
-                                    LoginPage(),
-                                  );
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 16, vertical: 16),
-                                  child: Text(
-                                    "Hesabınız var mı?",
-                                    style: GoogleFonts.openSans(
-                                      fontSize: 14,
-                                      color: Colors.white70,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(12),
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(5),
-                                onTap: () {
-                                  Get.back();
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.all(16),
-                                  child: Text(
-                                    "Eve Dön",
-                                    style: GoogleFonts.openSans(
-                                      fontSize: 14,
-                                      color: Colors.white70,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ]),
-              ),
-            );
-          },
-        ));
+                    ),
+                  ]),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.policy),
+      ),
+    );
   }
 }
